ed976e5cfa7790305e36857c130d6dce65077ab3
JoshdfG <agboolajoshua161@gmail.com>
2024-01-04 13:56:00 +0100

    modified:   server.js new file:   views/404.ejs modified:   views/about.ejs modified:   views/partials/header.ejs

--- server.js
+++ server.js
@@ -8,9 +8,16 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"))
 // Define a route to render your EJS file
 app.get('/', (req, res) => {
-  res.render('index'); 
+  res.render('index', {title:"Home"}); 
 });
 
+app.get("/about", (req,res)=>{
+  res.render('about', {title:"About"} )
+})
+
+app.use((req,res)=>{
+  res.status(404).render('404', {title:"404"})
+})
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
