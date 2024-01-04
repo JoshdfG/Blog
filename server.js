4fc927491b5236933874859676a98fc2fdd87e28
JoshdfG <agboolajoshua161@gmail.com>
2024-01-04 13:40:05 +0100

    modified:   server.js 	new file:   views/about.ejs 	modified:   views/index.ejs 	new file:   views/partials/navbar.ejs

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const app = express();
 const path = require("path")
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
-// // app.use(express.static('src'));
-// app.set("views",path.join(__dirname,"views")); 
 
 app.use(express.static("public"))
 // Define a route to render your EJS file
 app.get('/', (req, res) => {
-  res.render('index'); // Replace 'index' with your EJS file name (without the extension)
+  res.render('index'); 
 });
 
 // Start the server
